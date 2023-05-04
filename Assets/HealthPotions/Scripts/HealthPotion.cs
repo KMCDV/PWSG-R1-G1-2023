1993d854e3f1f4e0cbaadd62d858fae5ba665e9b
Krzysztof Maliński <krzysztof.malinski@cdv.pl>
2023-05-04 10:22:09 +0200

    HealthSystem

---
+++ Assets/HealthPotions/Scripts/HealthPotion.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Health Potion", menuName = "Brave Square/Health Potion")]
+public class HealthPotion : ScriptableObject
+{
+    [Header("ugabuga")]
+    public int healthToHeal;
+}
---
+++ Assets/HealthPotions/Scripts/HealthPotionContainer.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotionContainer : MonoBehaviour
+{
+    public HealthPotion HealthPotion;
+}
